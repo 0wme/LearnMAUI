66533a2e1030386487f2cd5dc0e01e65c5ac4b2b
Tom <skixoxgaming@gmail.com>
2024-02-23 18:37:41 +0100

    Ajoutez des fichiers projet.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LearnMAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
