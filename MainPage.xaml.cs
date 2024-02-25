0bc49f029d5f2060d36d809ec328917afefbb982
Tom <skixoxgaming@gmail.com>
2024-02-25 23:35:23 +0100

    Modification des MainPage

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,29 +1,29 @@
-﻿namespace LearnMAUI
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    int count = 0;
+
+    public MainPage()
     {
-        int count = 0;
-
-        public MainPage()
-        {
-            InitializeComponent();
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
-
-        private async void OnNavigateToSecondPageClicked(object sender, EventArgs e)
-        {
-            await Shell.Current.GoToAsync("///SecondPage");
-        }
+        InitializeComponent();
     }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+    private void OnNavigateClicked(object sender, EventArgs e)
+    {
+        Shell.Current.GoToAsync("//SecondPage");
+    }
+        
+        
 }
