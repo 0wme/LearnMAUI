729972c66072577318a4f0710e1a6bfc9f3fb12b
Tom <skixoxgaming@gmail.com>
2024-02-25 03:31:51 +0100

    Configuration de la 2eme page

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -20,6 +20,10 @@
 
             SemanticScreenReader.Announce(CounterBtn.Text);
         }
-    }
 
+        private async void OnNavigateToSecondPageClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("///SecondPage");
+        }
+    }
 }
